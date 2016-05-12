a0d1715c917755972ccd31bd60efdc8de170a850
Kevo <kevo548@gmail.com>
2016-05-12 15:20:33 -0400

    working on DB

--- CodeBloc/routes/html-routes.js
+++ CodeBloc/routes/html-routes.js
@@ -12,15 +12,35 @@ const router  = express.Router();
 
 // GET index.html on initial site load
 router.get("/", function(req, res, then) {
-	res.sendFile(path.join(process.cwd(), "public", "/index.html"));
-});
+	res.sendFile(path.join(process.cwd(), "public", "/index.html")); // routing for the main page, displayed will differ if user is logged in or not
+});                                                                  // IF user IS logged in, send to logged in user home page
 
 router.get("/register", function(req, res, then) {
-	res.sendFile(path.join(process.cwd(), "public", "/register.html"));
-});
+	res.sendFile(path.join(process.cwd(), "public", "/register.html")); // register button will only be seen if not logged in next to the login button
+});                                                                     // IF user ISN'T logged in, display register button next to login button with reduced info displayed
 
 router.get("/profile", function(req, res, then) {
-	res.sendFile(path.join(process.cwd(), "public", "/profile.html"))
+	res.sendFile(path.join(process.cwd(), "public", "/profile.html")); // profile page available for logged in users, can replace the log in button?
+});                                                                    // IF user IS logged in, display profile button
+
+router.get("/helproom", function(req, res, then) {
+	res.sendFile(path.join(process.cwd(), "public", "/helproom.html")); // message board to get/post help
+});                                                                     // possible working names: helpboard, helpzone
+
+router.get("/projectroom", function(req, res, then) {
+	res.sendFile(path.join(process.cwd(), "public", "/projectroom.html")); // different design than a message board specifically for projects? maybe images/descriptions that link to threads?
+});
+
+router.get("/panicroom", function(req, res, then) {
+	res.sendFile(path.join(process.cwd(), "public", "/panicroom.html")); // urgent help chat room, slack//freenode integration?
+});                                                                      //
+
+router.get("/about", function(req, res, then) {
+	res.sendFile(path.join(process.cwd(), "public", "/about.html"));
+});
+
+router.get("/contact", function(req, res, then) {
+	res.sendFile(path.join(process.cwd(), "public", "/contact.html"));
 });
 
 // middleware to catch 404s. can create or template custom 404 page
