a1575020466f21663e9f166e72f30e13ecd90f95
Kevo <kevo548@gmail.com>
2016-05-14 19:17:49 -0400

    after class commit

--- docs/schema.sql
+++ docs/schema.sql
@@ -16,14 +16,14 @@ CREATE TABLE users (
 CREATE TABLE `categories` (
 	`cat_id`          INT(8) NOT NULL AUTO_INCREMENT,
 	`cat_name`        VARCHAR(255) NOT NULL,
-	`cat_description`     VARCHAR(255) NOT NULL,
+	`cat_description` VARCHAR(255) NOT NULL,
 	UNIQUE INDEX `cat_name_unique` (`cat_name`),
 	PRIMARY KEY (`cat_id`)
 );
 
 CREATE TABLE `topics` (
 	`topic_id`        INT(8) NOT NULL AUTO_INCREMENT,
-	`topic_subject`       VARCHAR(255) NOT NULL,
+	`topic_subject`   VARCHAR(255) NOT NULL,
 	`topic_date`      DATETIME NOT NULL,
 	`topic_cat`       INT(8) NOT NULL,
 	`topic_by`        INT(8) NOT NULL,
@@ -32,7 +32,7 @@ CREATE TABLE `topics` (
 
 CREATE TABLE `posts` (
 	`post_id`         INT(8) NOT NULL AUTO_INCREMENT,
-	`post_content`        TEXT NOT NULL,
+	`post_content`    TEXT NOT NULL,
 	`post_date`       DATETIME NOT NULL,
 	`post_topic`      INT(8) NOT NULL,
 	`post_by`     INT(8) NOT NULL,
