a1642be1d7b0313127d83edc4af9b5bb916f25c9
Mariah McLaughlin <hello@mariahmclaughlin.com>
2016-05-19 00:12:58 -0400

    mariah merge. still working on login

--- CoderBloc/routes/user-routes.js
+++ CoderBloc/routes/user-routes.js
@@ -91,4 +91,76 @@ router.route("/api/users/:id")
 
 	});
 
+
+
+//Mariah noodlin space below:
+
+//this is also doing... nothing. seems to only be able to find by ID and nothing else
+//
+// router.route("/api/users/:userName")
+// 	.get(function(req,res, next) {
+// 			console.log(req.params.userName);
+// 		var response = {};
+//
+// 		Posts.findAll({
+// 			where: { userName: req.params.userName }
+// 		}).then(function(data) {
+//
+// 				response = {"error" : false, "data" : data}; // place data in the response obj
+// 				res.json(response); // display the response obj on the page
+// 				console.log(response);
+//
+// 			}).catch(function (err) { // this will catch any errors that occur in our query chain
+//
+// 			response = {"error" : true, "data" : "ERROR " + err}; // puts any errors in the response obj
+// 			res.json(response);
+// 			console.error('SEQUELIZE ERROR: ', err);
+// 			return done(err);
+//
+// 		});
+//
+// 	}) // UPDATE POST data with a chained route ~ user post editing
+// 	.put(function(req,res, next){
+//
+//
+//
+// 	}) // DELETE POST data with a chained route ~ admin only, users shouldn't have access
+// 	.delete(function(req,res, next){
+//
+//
+//
+// 	});
+
+
+
+////tried alt approach, but stil nothing
+
+// router.get('/api/users/:userName?', function(req, res){
+//
+// 	// If the user provides a specific character in the URL...
+// 	if(req.params.userName){
+//
+// 		// Then display the JSON for ONLY that character.
+// 		// (Note how we're using the ORM here to run our searches)
+// 		Users.findAll({
+// 			where: {
+// 				userName: req.params.userName
+// 			}
+// 		}).then(function(result){
+// 			res.json(result);
+// 		})
+// 	}
+//
+// 	// Otherwise...
+// 	else{
+// 		// Otherwise display the data for all of the characters.
+// 		// (Note how we're using Sequelize here to run our searches)
+// 		Users.findAll({})
+// 			.then(function(result){
+// 				res.json(result);
+// 			})
+// 	};
+//
+// });
+
 module.exports = router;
