34d891e3e95388e54affb30a483bde8e1c333bef
Mariah McLaughlin <hello@mariahmclaughlin.com>
2016-05-18 21:41:56 -0400

    still working on login

--- CodeBloc/controllers/sign-up-ctrl.js
+++ CodeBloc/controllers/sign-up-ctrl.js
@@ -2,6 +2,7 @@
  * Created by CodeBloc on 5/11/2016.
  */
 
+
 // !!!!ANGULAR IS CLIENTSIDE AND CONSOLE LOGS TO THE CHROME CONSOLE BY DEFAULT!!!!
 // var moment = require('moment');
 var app = angular.module("CodeBloc", []);
@@ -37,3 +38,51 @@ var app = angular.module("CodeBloc", []);
 		};
 
 	}]); //end app controller
+
+
+//controller for LOGIN
+
+
+app.controller("loginCtrl", ["$scope","$http","$window", function($scope, $http, $window){
+	//check this out in the modal! Captures the information
+	$scope.loginData= {};
+
+	$scope.login = function(){ // declare an obj to hold the user data from the sign up form
+
+
+		var logData = { // grab the data from the form and get it ready for DB insertion
+			userName : $scope.loginData.userName,
+			userPass : $scope.loginData.pass
+		};
+
+		console.log(logData); // works
+
+
+		$http.get("/api/users", logData) // makes this data available in the http req
+			.then(function(req, res){ // after successful post of new user
+
+				console.log(logData.userName);
+				// console.log(data);
+				console.log(req);
+				// alert("POST WORKING");
+				// $window.location.assign("/index.html"); // redirect to index.html with user landing features
+
+				// if($scope.logData.userName === req.params.userName){
+				// 	alert('name found!');
+				//
+				// 	//Mariah: adding usr info when they've successfully registered
+				// 	$scope.userLogInfo = "Welcome back, " + logData.userName;
+				// }
+
+
+			})
+			.error(function(data){
+				console.log("Error: "+ data);
+				alert('oooo');
+			});
+
+
+
+	};
+
+}]); //end app controller
--- CodeBloc/routes/api-routes.js
+++ CodeBloc/routes/api-routes.js
@@ -69,7 +69,7 @@ router.route("/api/users/:id")
 
 		var response = {};
 
-		Users.findById(req.params.id, options)
+		Users.findById(req.params.id)
 			.then(function(data) {
 				response = {"error" : false, "data" : data}; // place data in the response obj
 				res.json(response); // display the response obj on the page
