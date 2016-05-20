abb61a3368df5f317ae54f31758ef5b74f19a84f
Kevo <kevo548@gmail.com>
2016-05-20 07:06:20 -0400

    basic login mechanics complete

--- CoderBloc/controllers/sign-up-ctrl.js
+++ CoderBloc/controllers/sign-up-ctrl.js
@@ -25,7 +25,7 @@ var app = angular.module("CoderBloc", []);
 			   .then(function(data){ // after successful post of new user
 
 				   //Mariah: adding usr info when they've successfully registered
-				   $scope.userLogInfo = "Thank you for registering, " + userData.userName;
+				   $scope.userLogInfo = "Thank you for registering, " + userData.userName + ". You may now Login";
 
 				// alert("POST WORKING");
 				// $window.location.assign("/index.html"); // redirect to index.html with user landing features
@@ -44,49 +44,70 @@ var app = angular.module("CoderBloc", []);
 //controller for LOGIN:
 
 
-app.controller("loginCtrl", ["$scope","$http","$window", function($scope, $http, $window){
-	//check this out in the modal! Captures the information
-	$scope.loginData= {};
+	app.controller("loginCtrl", ["$rootScope","$scope","$http","$window","$timeout", function($rootScope, $scope, $http, $window, $timeout){
+		//check this out in the modal! Captures the information
+		$scope.loginData= {};
 
-	$scope.login = function(){ // declare an obj to hold the user data from the sign up form
-
-
-		var logData = { // grab the data from the form and get it ready for DB insertion
-			userName : $scope.loginData.userName,
-			userPass : $scope.loginData.pass
-		};
-
-		console.log(logData); // works
+		// $scope.loginDataDismiss = "modal";
 
 
-		$http.get("/api/users/name/"+ $scope.loginData.userName) // makes this data available in the http req
-			.then(function(data){ // after successful post of new user
+			$rootScope.open = function() {
+				$scope.displayLoginModal = true
+			};
 
-				var loginName = $scope.loginData.userName;
-
-
-				// console.log(dbName);
-				//if there's no data, it means the user name is incorrect, else, logged in
-				//once we get the pw check implemnted
-				if(data.data.data === null){
-
-					console.log(' incorrect username');
-
-					//TASK: create a errormessage on the modal when incorrect name
-
-				}else if(loginName === data.data.data.userName){
-					console.log('yay logged in');
-				}
+		// $scope.displayLoginModal = false
 
 
-
-			})
-			.error(function(data){
-				console.log("Error: "+ data);
-				alert('oooo');
-			});
+		$scope.login = function(){ // declare an obj to hold the user data from the sign up form
 
 
+			var logData = { // grab the data from the form and get it ready for DB insertion
+				userName : $scope.loginData.userName,
+				userPass : $scope.loginData.pass
+			};
+
+			console.log(logData); // works
+
+
+			$http.get("/api/users/name/"+ $scope.loginData.userName) // makes this data available in the http req
+				.then(function(data){ // after successful post of new user
+
+					var loginName = $scope.loginData.userName;
+
+
+					// console.log(dbName);
+					//if there's no data, it means the user name is incorrect, else, logged in
+					//once we get the pw check implemnted
+					if(data.data.data === null){
+
+						$scope.loginStatusDiv = "Sorry, User Not Found";
+
+						console.error(' incorrect username');
+
+						//TODO: create a errormessage on the modal when incorrect name
+
+					}
+
+					else if(loginName === data.data.data.userName){ // IF user credentials are VALID
+
+						// called with a 3 second timeout below. hides the modal and displays welcome messages. 
+						// can add other landing page features here in the future
+						function successLogin() {
+							angular.element('.modal').css('display', 'none');
+							$rootScope.userLogInfo = "Welcome to CoderBloc, " + loginName + "!";
+						}
+
+						$scope.loginStatusDiv = "Successful Login!";
+						$timeout(successLogin, 3000);
+						console.log('yay logged in');
+
+					}
+
+				})
+				.error(function(data){
+					console.log("Error: "+ data);
+					alert('oooo');
+				});
 
 
 	};//end login function
