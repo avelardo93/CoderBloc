90727e12a92e5824724c1abdb1b3f9c0eac97dfd
Kevo <kevo548@gmail.com>
2016-05-25 19:02:27 -0400

    testing thread layouts

--- CoderBloc/controllers/core.js
+++ CoderBloc/controllers/core.js
@@ -2,4 +2,4 @@
  * Created by Kevo on 5/25/2016.
  */
 
-angular.module("CoderBloc", ["userCtrl"]);
+angular.module("CoderBloc", ["userCtrl", "threadCtrl"]);
--- CoderBloc/controllers/thread-ctrl.js
+++ CoderBloc/controllers/thread-ctrl.js
@@ -2,35 +2,56 @@
  * Created by Kevo on 5/20/2016.
  */
 
-var app = angular.module("CoderBloc", []);
+var app = angular.module("threadCtrl", []);
 
-app.controller("threadCtrl", ["$scope","$http","$window", function($scope, $http, $window){
+user.controller("helpThreadCtrl", ["$scope","$http","$window", function($scope, $http, $window){
 
-	$scope.formData= {};
+	$scope.catName = "Looking For Help";
 
-	$scope.createUser = function(){ // declare an obj to hold the user data from the sign up form
+	// $scope.threadCreate = function(){
+	// 	var threadData = {
+	// 		threadName: $scope.data.newThread
+	// 	}
+	// };
+
+	// console.log(threadData);
+
+	$http.get("/api/cat1/threads") // makes this data available in the http req including the password. how can i mask this?
+		.then(function(data){ // after getting user credentials
+
+			$scope.threads = data.data.data;
+
+			for (var i = 0; i < $scope.threads.length; i++) {
+				console.log($scope.threads[i]);
+				$scope.threadNames = $scope.threads[i].threadName
+			}
+
+			// $scope.threadTable = new NgTableParams({
+			// 	page: 1,
+			// 	count: 0
+			// }, {
+			// 	total: $scope.threads.length,
+			// 	getData: function ($defer, params) {
+			// 		$scope.data = $scope.threads.slice((params.page() - 1) * params.count(), params.page() * params.count());
+			// 		$defer.resolve($scope.data);
+			// 	}
+			// });
 
 
-		var userData = { // grab the data from the form and get it ready for DB insertion
-			userEmail: $scope.formData.userEmail,
-			userName : $scope.formData.userName,
-			userPass : $scope.formData.userPass
-		};
 
-		$http.post("/api/users", userData) // makes this data available in the http req
-			.then(function(data){ // after successful post of new user
+			console.log(data.data.data);
 
-				//Mariah: adding usr info when they've successfully registered
-				$scope.userLogInfo = "Thank you for registering, " + userData.userName + ". You may now Login";
+			$scope.threadName = data.data.data[0].threadName;
 
-				// alert("POST WORKING");
-				// $window.location.assign("/index.html"); // redirect to index.html with user landing features
 
-			})
-			.error(function(data){
-				console.log("Error: "+ data);
-			});
-
-	};
+			//
+			// 	if(data) {
+			// 		console.log(data);
+			// 		$scope.catName = data;
+			// 	}
+			// 	})
+			// 	.error(function(data){
+			// 		console.log("Error: "+ data);
+		});
 
 }]); //end app controller
